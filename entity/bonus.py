859dcef9f5ca17ba1c7a32a1e1d0df36cee8e48f
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2022-01-26 13:43:10 +0200

    Update bonus.py

--- entity/bonus.py
+++ entity/bonus.py
@@ -3,11 +3,12 @@ from entity.tasks import Tasks
 
 class Bonus(Tasks):
 
-	def __init__(self,id=None, task, reward):
-		self.id = id # task has id, do i need here?
+	def __init__(self, id=None, task:Tasks=None, reward=None):
+		self.id = id
 		self.task = task
 		self.reward = reward
 
 	def __str__(self):
 		return f"| {self.task.name:<20.20} | {self.task.category:<15.15} | {self.task.minutes:<2d} min | {self.reward:<10.10} |"
+
 
