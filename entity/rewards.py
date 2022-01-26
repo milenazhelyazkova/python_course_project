d9af5af90825b652c810dd53f0ac7abed290c14f
Milena Zhelyazkova <76998660+milenazhelyazkova@users.noreply.github.com>
2022-01-26 13:27:49 +0200

    Update rewards.py

--- entity/rewards.py
+++ entity/rewards.py
@@ -1,14 +1,14 @@
-from entity.tasks import Tasks
 
-
-class Rewards(Tasks):
-	def __init__(self, reward):
-		super().__init__()
-		self.reward = reward
-
+class Rewards:
+	def __init__(self, id=None, name=None, category=None, owners=tuple(), description=None):
+		self.id = id
+		self.name = name
+		self.category = category
+		self.owners = owners
+		self.description = description
 
 	def __str__(self):
 
-		return 	f"| {self.reward.name:<12.12}  | {self.reward.category:<15.15} | " \
-		  		f"{', '.join(self.reward.owners):<20.20} | " \
-		   		f"{self.reward.description:<50.50} |"
+		return 	f"| {self.name:<12.12}  | {self.category:<15.15} | " \
+		  		f"{', '.join(self.owners):<20.20} | " \
+		   		f"{self.description:<50.50} |"
